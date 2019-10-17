814ac2003fa2126450c6e49421f6232e696f50d9
龚泓旭 <fiendo@foxmail.com>
2019-10-17 18:08:06 +0800

    add eureka zuul helloworld demo

---
+++ service-helloworld/src/main/java/com/fiendo/Application.java
@@ -0,0 +1,19 @@
+package com.fiendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+/**
+ * @author fiendo
+ * @version 1.0 (2019/10/17)
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
