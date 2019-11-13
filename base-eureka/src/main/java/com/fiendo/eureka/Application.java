84c360cd3e6105cd6c21af4eccc6db8f39a61112
fiendoghx <fiendo@foxmail.com>
2019-11-13 19:15:40 +0800

    update

--- base-eureka/src/main/java/com/fiendo/eureka/Application.java
+++ base-eureka/src/main/java/com/fiendo/eureka/Application.java
@@ -1,13 +1,13 @@
 package com.fiendo.eureka;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        new SpringApplicationBuilder(Application.class).run(args);
     }
 }
--- base-gateway/src/main/java/com/sendi/gateway/Application.java
+++ base-gateway/src/main/java/com/sendi/gateway/Application.java
@@ -1,17 +1,13 @@
-package com.fiendo;
+package com.sendi.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-/**
- * @author fiendo
- * @version 1.0 (2019/10/17)
- */
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
